90089df0fb9db3a6e9da3e7e2ed1e5ffc867a17e
Azur Rosenlöv <azur.rosenlov@solita.fi>
2021-09-10 10:06:56 +0200

    feat: Added functionality that exports data to .csv file

--- Fingrid_comparison.py
+++ Fingrid_comparison.py
@@ -1,9 +1,13 @@
 import numpy as np 
 import pandas as pd
+import datetime
+#import json
+import csv
 
+time = datetime.date.today()-datetime.timedelta(days=1)
 def reader():
-    wind_prod = pd.read_json('C:/Users/azur.rosenlov/Desktop/Data_Academy_Python/Wind generation in 2021-09-07.json')
-    elec_cons = pd.read_json('C:/Users/azur.rosenlov/Desktop/Data_Academy_Python/Electricity consumption in 2021-09-07.json')
+    wind_prod = pd.read_json('C:/Users/azur.rosenlov/Desktop/Data_Academy_Python/Wind generation in {}.json'.format(str(time)))
+    elec_cons = pd.read_json('C:/Users/azur.rosenlov/Desktop/Data_Academy_Python/Electricity consumption in {}.json'.format(str(time)))
     return wind_prod, elec_cons
 
 reader() 
@@ -22,4 +26,13 @@ def percent_share():
     result_format = '{:.0%}'.format(share_elec)
     return result_format
 
-print('The share of the total electricity consumption that can come from wind generated electricity is: ' + str(percent_share()))
+dict_result = {'Date' : str(time), 'Percentage': percent_share()}
+dict_keys = list(dict_result)
+
+#with open('Daily share wind generation and power consumption', 'a') as f:
+#    f.write(json.dumps(dict_result))
+
+with open('C:/Users/azur.rosenlov/Desktop/Data_Academy_Python/Fingrid_csv', 'a', encoding='UTF8') as f:
+    out = csv.DictWriter(f, fieldnames= dict_keys)
+    out.writeheader()
+    out.writerow(dict_result)
